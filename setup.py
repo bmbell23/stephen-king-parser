b28f029c7f6a40ec2379c79fa5050aaff2071313
brandon bell <bbell@ddn.com>
2025-02-19 14:31:03 +0000

    Update version to 3.0.2 across all files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="stephen-king-parser",
-    version="0.1.0",
+    version="3.0.2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
